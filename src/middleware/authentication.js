f0065a1210c38597fa2e777a8592c34263ff5e75
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-09-07 09:59:57 -0700

    Replace 401 response with 403 response

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -4,7 +4,7 @@ const authenticationMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     console.log('No token provided');
-    return res.status(401).send("Unauthenticated")
+    return res.status(403).send("Unauthenticated")
   };
   const token = authHeader.split(' ')[1];
 
@@ -15,7 +15,7 @@ const authenticationMiddleware = async (req, res, next) => {
     next();
   } catch (error) {
     console.log('Not authed to access this route');
-    return res.status(401).send("Unauthenticated")
+    return res.status(403).send("Unauthenticated")
   }
 };
 
