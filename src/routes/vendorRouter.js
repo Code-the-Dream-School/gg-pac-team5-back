e3fe1c523ba2a512c9af0fc49b371f6077ed1a33
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-09-09 15:51:10 -0700

    Add method to get vendor by name

--- src/routes/vendorRouter.js
+++ src/routes/vendorRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const vendorController = require('../controllers/vendorController.js');
 const authenticateUser = require('../middleware/authentication.js');
 
+router.route('/name/:name').get(vendorController.getVendorByName);
 router.route('/').get(vendorController.getAllVendors).post(authenticateUser, vendorController.createVendor);
 router.route('/:id').get(vendorController.getVendor).patch(authenticateUser, vendorController.updateVendor).delete(authenticateUser, vendorController.deleteVendor);
 
