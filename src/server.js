328c0fb84744dc98ae9c08aa0841cf991b54f825
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-09-12 15:42:57 -0700

    Allow reseeding the database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const { PORT = 8000 } = process.env;
 const app = require("./app");
 const connectDB = require("./db/connect");
-// const populateTestData = require('./utils/populateTestData');
+const populateTestData = require('./utils/populateTestData');
 
 (async () => {
 	try {
 		await connectDB(process.env.MONGO_URI, console.log("Connected to the DB"));
-		// await populateTestData();
+		await populateTestData();
 		const listener = () => console.log(`Listening on Port ${PORT}!`);
 		app.listen(PORT, listener);
 	} catch (error) {
